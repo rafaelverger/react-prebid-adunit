9e9c2a2663ba6f5f9fd8ce5acdd4f3bdfca4f71a
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2017-07-11 16:21:31 +0100

    Using new Prebid generic container in DFP

--- demo/client.js
+++ demo/client.js
@@ -3,8 +3,8 @@ import { DFPPrebidContainer } from '../src';
 ReactDOM.render(
   React.createElement('div', {
     children: [
-      React.createElement(DFPAdContainer, {
-        id: 'div-gpt-ad-1460505748561-0',
+      React.createElement(DFPPrebidContainer, {
+        domID: 'div-gpt-ad-1460505748561-0',
         slot: 'header-bid-tag-0',
         dimensions: [[300, 250], [300,600]],
         adNetwork: '19968336',
@@ -15,8 +15,8 @@ ReactDOM.render(
           }
         }]
       }),
-      React.createElement(DFPAdContainer, {
-        id: 'div-gpt-ad-1460505661639-0',
+      React.createElement(DFPPrebidContainer, {
+        domID: 'div-gpt-ad-1460505661639-0',
         slot: 'header-bid-tag1',
         dimensions: [[728, 90], [970, 90]],
         adNetwork: '19968336',
--- src/dfp.jsx
+++ src/dfp.jsx
@@ -1,53 +1,30 @@
-import { Component } from 'react';
+import PrebidContainer from './_prebid';
 import PropTypes from 'prop-types';
-import { nautilus } from 'nautilusjs';
-
-const DFP_URL = '//www.googletagservices.com/tag/js/gpt.js';
-const PREBID_URL = '//acdn.adnxs.com/prebid/not-for-prod/prebid.js';
-const ADSERVER_TIMEOUT = 2000;
 
 const propTypes = {
-  id: PropTypes.string.isRequired,
   slot: PropTypes.string.isRequired,
   adNetwork: PropTypes.string.isRequired,
-  bids: PropTypes.arrayOf(PropTypes.shape({
-    bidder: PropTypes.string.isRequired,
-    params: PropTypes.shape({
-      placementId: PropTypes.string.isRequired,
-    }),
-  })),
-  dimensions: PropTypes.arrayOf(PropTypes.arrayOf(PropTypes.number)).isRequired,
-  className: PropTypes.string,
   targeting: PropTypes.object,
   afterAdLoaded: PropTypes.func,
   flexible: PropTypes.bool,
-  skipIf: PropTypes.func,
 };
 
 const defaultProps = {
   targeting: {},
-  skipIf: () => false,
   afterAdLoaded: () => {},
 };
 
 
-export default class DFPPrebidContainer extends Component {
-  static get propTypes() { return propTypes; }
-  static get defaultProps() { return defaultProps; }
+export default class DFPPrebidContainer extends PrebidContainer {
+  static get propTypes() { return { ...PrebidContainer.propTypes, ...propTypes }; }
+  static get defaultProps() { return { ...PrebidContainer.defaultProps, ...defaultProps }; }
 
   constructor(props) {
     super(props);
-    this.state = { hide: true, domID: (props.id || props.slot) };
-    this.defineSlot = ::this.defineSlot;
     this.slotRendered = ::this.slotRendered;
-    this.sendAdserverRequest = ::this.sendAdserverRequest;
   }
 
-  componentDidMount() {
-    const { skipIf } = this.props;
-    if (skipIf()) return;
-    console.log(this.props, this.state);
-
+  adServerInit(window) {
     if (!window.googletag) {
       console.log('mocking dfp', this.state);
       window.googletag = { __slots: [], cmd: [], };
@@ -56,21 +33,17 @@ export default class DFPPrebidContainer extends Component {
         googletag.pubads().disableInitialLoad();
       });
     }
-
-    if (!window.pbjs) {
-      window.pbjs = { que: [] };
-      window.pbjsAdServerTimeout = setTimeout(this.sendAdserverRequest, ADSERVER_TIMEOUT);
-      nautilus([PREBID_URL], [DFP_URL]);
-    }
-
-    this.defineSlot(window);
   }
 
-  sendAdserverRequest() {
-    console.log('sendAdserverRequest', this.state);
-    if (window.pbjs.adserverRequestSent) return;
-    clearTimeout(window.pbjsAdServerTimeout);
-    window.pbjs.adserverRequestSent = true;
+  adServerType() {
+    return 'dfp';
+  }
+
+  adServerURL() {
+    return '//www.googletagservices.com/tag/js/gpt.js';
+  }
+
+  adServerRequest(window) {
     window.googletag.cmd.push(() => {
       console.log('set target');
       window.pbjs.setTargetingForGPTAsync();
@@ -79,22 +52,10 @@ export default class DFPPrebidContainer extends Component {
     });
   }
 
-  defineSlot({ pbjs, googletag }) {
-    const { adNetwork, slot, dimensions, bids, targeting } = this.props;
+  adServerSlot(window, isTheLastSpot, lastSpotCallback) {
+    const { pbjs, googletag } = window;
+    const { adNetwork, slot, dimensions, targeting, domID } = this.props;
     const adunitPath = `/${adNetwork}/${slot}`;
-    const domID = this.state.domID;
-    if (googletag.__slots.indexOf(domID) > -1) return;
-
-    // googletag.__slots is used to assert that slots are loaded one time only
-    googletag.__slots.push(domID);
-    const allSpotsOnPage = document.querySelectorAll('[data-prebid-adspot');
-    const isTheLastSpot = allSpotsOnPage.length == googletag.__slots.length;
-
-    // prebid addUnit
-    pbjs.que.push(() => {
-      console.log('pbjs.addAdUnits', { code: domID, sizes: dimensions, bids });
-      pbjs.addAdUnits([{ code: domID, sizes: dimensions, bids }])
-    });
 
     // googletag.cmd is an array of commands that will be execute in inserted order after DFP loads
     googletag.cmd.push(() => {
@@ -117,27 +78,22 @@ export default class DFPPrebidContainer extends Component {
       // googletag.display(domID); // #googletag.display
       // #googletag.Service_addEventListener
       pubadsService.addEventListener('slotRenderEnded', this.slotRendered);
-      if (isTheLastSpot){
+      if (isTheLastSpot) {
         console.log('enableSingleRequest // enableServices');
         googletag.pubads().enableSingleRequest();
         googletag.enableServices();
         console.log('pbjs.requestBids');
-        pbjs.requestBids({
-          bidsBackHandler: this.sendAdserverRequest
-        });
+        lastSpotCallback();
       }
     });
   }
 
   slotRendered(event) {
-    const { props: { flexible }, state: { domID } } = this;
+    const { flexible, domID } = this.props;
     console.log(flexible, domID, event.slot.getSlotId().getDomId(), event.isEmpty);
     if (event.slot.getSlotId().getDomId() !== domID) return;
 
-    const adspot = document.getElementById(domID);
-    // event.isEmpty returns true if there's no ads to show in this slot
-    this.setState({ hide: event.isEmpty });
-    this.props.afterAdLoaded(!event.isEmpty, event.size, adspot);
+    this.props.afterAdLoaded(!event.isEmpty, event.size);
     if (flexible && !event.isEmpty) {
       const adFrame = adspot.querySelector('iframe');
       const { parentNode: container, contentDocument: adDocument } = adFrame;
@@ -151,10 +107,4 @@ export default class DFPPrebidContainer extends Component {
       updateHeight();
     }
   }
-
-  render() {
-    const { props: { className }, state: {domID} } = this;
-    return <div id={domID} className={`adspot ${className}`} data-prebid-adspot />;
-  }
-
 }
