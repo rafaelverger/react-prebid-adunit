952c50ea038ab60e33ea91587e22530cef32da21
Rafael Nunes Verger <rafael@rafaelverger.com.br>
2017-07-21 16:44:26 +0100

    Improving demo page styles and increase adexchanges

--- demo/client.js
+++ demo/client.js
@@ -6,16 +6,22 @@ ReactDOM.render(
   React.createElement('div', {
     children: [
       React.createElement(DFPPrebidContainer, {
-        domID: 'Bloco_Diarios_View_Docside_elm0',
-        slot: 'Bloco_Diarios_View_Docside',
+        domID: 'Diarios_View_Docside_elm0',
+        slot: 'Diarios_View_Docside',
         dimensions: [[300, 250], [300,600]],
         adNetwork: '1045205',
         bids: [
           {
             bidder: 'aol',
             params: {
-              placement: '4601503',
+              placement: '4601506',
               network: '11111.1'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797453'
             }
           },
           {
@@ -33,10 +39,160 @@ ReactDOM.render(
         ]
       }),
       React.createElement(DFPPrebidContainer, {
-        domID: 'Bloco_Diarios_View_Docside_elm1',
-        slot: 'Bloco_Diarios_View_Docside',
-        dimensions: [[300,600]],
-        adNetwork: '1045205'
+        domID: 'Diarios_View_Toppage_elm1',
+        slot: 'Diarios_View_Toppage',
+        dimensions: [[728,90],[970,90], [970,250]],
+        adNetwork: '1045205',
+        bids: [
+          {
+            bidder: 'aol',
+            params: {
+              placement: '4601505',
+              network: '11111.1'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797446'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797462'
+            }
+          },
+        ]
+      }),
+      React.createElement(DFPPrebidContainer, {
+        domID: 'Diarios_View_Innerpage_elm2',
+        slot: 'Diarios_View_Innerpage',
+        dimensions: [[728,90],[970,90], [970,250]],
+        adNetwork: '1045205',
+        bids: [
+          {
+            bidder: 'aol',
+            params: {
+              placement: '4601508',
+              network: '11111.1'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797447'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797463'
+            }
+          },
+        ]
+      }),
+      React.createElement(DFPPrebidContainer, {
+        domID: 'Diarios_View_Bottompage_elm3',
+        slot: 'Diarios_View_Bottompage',
+        dimensions: [[728,90],[970,90], [970,250]],
+        adNetwork: '1045205',
+        bids: [
+          {
+            bidder: 'aol',
+            params: {
+              placement: '4601507',
+              network: '11111.1'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797448'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797464'
+            }
+          },
+        ]
+      }),
+      React.createElement(DFPPrebidContainer, {
+        domID: 'Diarios_View_Toppage_Mobile_elm4',
+        slot: 'Diarios_View_Toppage_Mobile',
+        dimensions: [[320,50], [320,100], [300,50], [300,100]],
+        adNetwork: '1045205',
+        bids: [
+          {
+            bidder: 'aol',
+            params: {
+              placement: '4601511',
+              network: '11111.1'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797456'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797458'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797460'
+            }
+          },
+        ]
+      }),
+      React.createElement(DFPPrebidContainer, {
+        domID: 'Diarios_View_Innerpage_Mobile_elm5',
+        slot: 'Diarios_View_Innerpage_Mobile',
+        dimensions: [[300,250]],
+        adNetwork: '1045205',
+        bids: [
+          {
+            bidder: 'aol',
+            params: {
+              placement: '4601509',
+              network: '11111.1'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797451'
+            }
+          },
+        ]
+      }),
+      React.createElement(DFPPrebidContainer, {
+        domID: 'Diarios_View_Bottompage_Mobile_elm6',
+        slot: 'Diarios_View_Bottompage_Mobile',
+        dimensions: [[300,250]],
+        adNetwork: '1045205',
+        bids: [
+          {
+            bidder: 'aol',
+            params: {
+              placement: '4601510',
+              network: '11111.1'
+            }
+          },
+          {
+            bidder: 'criteo',
+            params: {
+              zoneId: '797452'
+            }
+          },
+        ]
       })
     ]
   }),
